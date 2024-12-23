fe261ee653e32d06f1fca6c23700d5b3a4335cb4
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-23 22:09:47 +0530

    added: StockNews.js

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,6 @@
 import Hero from "@/components/Home/Hero";
 import LatestNews from "@/components/Home/LatestNews";
+import StockNews from "@/components/Home/StockNews";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
@@ -11,8 +12,9 @@ const home = () => {
 
       <div className="flex justify-between flex-wrap px-3 md:px-10 lg:px-16 py-12">
         {/* left  */}
-        <div className="w-full md:w-[68%]">
+        <div className="w-full md:w-[68%] flex flex-col space-y-20">
           <LatestNews />
+          <StockNews />
         </div>
         {/* right  */}
         <div className="w-full md:w-[28%]"></div>
