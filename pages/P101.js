3e0bc5d3e1596c3a3bddfd64404f80bb7aea0aed
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-23 18:24:14 +0530

    pages created

---
+++ pages/P101.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const P101 = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default P101;
---
+++ pages/news.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const news = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default news;
