f75c897bfe04a6ef7e484fbcbd83dc5f437d5370
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-25 14:57:27 +0530

    responsive: TabsSection.js

--- components/Investment/TabsSection.js
+++ components/Investment/TabsSection.js
@@ -39,14 +39,14 @@ const TabsSection = () => {
   return (
     <div className="bg-white border-t border-gray-200">
       <div className="container mx-auto px-3 md:px-12">
-        <ul className="flex items-center gap-x-8 md:gap-x-9 py-7 bg-white shadow-sm">
+        <ul className="flex flex-wrap items-center gap-x-5 py-6 bg-white shadow-sm">
           {tabs.map((tab, index) => (
             <li
               key={index}
               onClick={() => handleTabClick(index, tab.query)}
-              className={`cursor-pointer font-medium text-sm ${
+              className={`cursor-pointer font-medium text-sm  px-4 py-2 rounded-full mb-2 md:mb-0 ${
                 activeTab === index
-                  ? "text-black px-4 py-2 bg-secondary/15 rounded-full"
+                  ? "text-white bg-accent"
                   : "text-gray-700 hover:text-gray-900"
               }`}
             >
