e5eb78613cf20837fc98b5d8f720a608e0cccf2b
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-25 12:37:57 +0530

    sections added in news page

--- pages/news.js
+++ pages/news.js
@@ -1,3 +1,8 @@
+import DailyNewsletterAd from "@/components/Home/DailyNewsletterAd";
+import LatestNews from "@/components/Home/LatestNews";
+import StockNews from "@/components/Home/PressReleaseNews";
+import PressReleaseNews from "@/components/Home/PressReleaseNews";
+import Substacks from "@/components/Home/Substacks";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/News/Hero";
 import React from "react";
@@ -9,6 +14,20 @@ const news = () => {
       <div className="mt-14">
         {" "}
         <Hero />
+      </div>
+
+      <div className="flex justify-between flex-wrap px-3 md:px-10 lg:px-16 py-12 mt-6">
+        {/* left  */}
+        <div className="w-full md:w-[66%] flex flex-col space-y-20">
+          <LatestNews />
+          <StockNews />
+          <PressReleaseNews />
+        </div>
+        {/* right  */}
+        <div className="w-full md:w-[26%] space-y-20">
+          <DailyNewsletterAd />
+          <Substacks />
+        </div>
       </div>
     </div>
   );
