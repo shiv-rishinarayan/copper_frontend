44cad3e8c164bc72302d492ac975ddb7b58a0c42
Vaibhav <vaibhavjam86@gmail.com>
2024-12-25 12:27:38 +0530

    added hero section

--- pages/news.js
+++ pages/news.js
@@ -1,10 +1,15 @@
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/News/Hero";
 import React from "react";
 
 const news = () => {
   return (
     <div>
       <Navbar />
+      <div className="mt-14">
+        {" "}
+        <Hero />
+      </div>
     </div>
   );
 };
