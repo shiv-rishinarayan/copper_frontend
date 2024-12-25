9c9ec779ad1dc7de5fb4b5760e37f00032955185
Yogesh <coolcoder9334@gmail.com>
2024-12-25 20:48:14 +0530

    updated the the loading state globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Loader from "@/components/Loader";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+    const handleError = () => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleError);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleError);
+    };
+  }, [router]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return <Component {...pageProps} />;
 }
