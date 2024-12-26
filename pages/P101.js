9b9c377ec88637e8a934546f87061d6fd5495502
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-26 14:57:17 +0530

    P101 Hero section added

--- pages/P101.js
+++ pages/P101.js
@@ -1,10 +1,17 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import P101Hero from "@/components/P101/P101Hero";
 import React from "react";
 
 const P101 = () => {
   return (
     <div>
       <Navbar />
+      <P101Hero />
+
+      <div className="mt-24">
+        <Footer />
+      </div>
     </div>
   );
 };
