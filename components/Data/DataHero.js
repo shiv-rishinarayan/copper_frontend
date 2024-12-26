62f50d0c53419c2c9b5a4192664798cce60e1885
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-26 14:46:07 +0530

    data page tabs setup added

---
+++ components/Data/DataHero.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const DataHero = () => {
+  return (
+    <div
+      className="relative w-full bg-secondary/0 py-16 md:py-32 lg:py-44 bg-cover bg-top"
+      style={{
+        backgroundImage:
+          "url(https://www.garfieldrefining.com/wp-content/uploads/2021/07/platinum-scaled.jpeg)",
+      }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-r from-black/90 to-black/50 bg-opacity-70 "></div>
+
+      <div className="max-w-7xl mx-auto relative container px-4 sm:px-6 md:px-12 z-10 pt-14 md:pt-12">
+        <div className="w-full md:w-[60%] lg:w-[55%]">
+            
+          <h1 className="text-2xl sm:text-3xl md:text-[3rem] lg:text-[3.1rem] font-bold text-white leading-tight sm:leading-[1.15]">
+            Discover the data you need to analyse and research Platinum
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DataHero;
---
+++ components/Data/TabSection.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+const TabsSection = () => {
+  const tabs = [
+    { name: "Price & Premiums", query: "priceandpremiums" },
+    { name: "Demand Database", query: "demanddatabase" },
+  ];
+
+  const router = useRouter();
+  const { tab } = router.query; // Get the current tab from query parameters
+
+  const [activeTab, setActiveTab] = useState(0);
+
+  useEffect(() => {
+    // Set active tab based on the query parameter
+    const activeIndex = tabs.findIndex((t) => t.query === tab);
+    if (activeIndex >= 0) {
+      setActiveTab(activeIndex);
+    } else {
+      setActiveTab(0); // Default to the first tab if no query is present
+    }
+  }, [tab]);
+
+  const handleTabClick = (index, query) => {
+    setActiveTab(index); // Update active tab
+    router.push(
+      {
+        pathname: router.pathname, // Keep the current path
+        query: { tab: query }, // Add or update the `tab` query parameter
+      },
+      undefined,
+      { shallow: true } // Avoid full page reload
+    );
+  };
+
+  return (
+    <div className="bg-white border-t border-gray-200">
+      <div className="container px-3 md:px-12">
+        <ul className="flex flex-wrap justify-start items-center gap-x-3 sm:gap-x-5 py-6 bg-white shadow-sm">
+          {tabs.map((tab, index) => (
+            <li
+              key={index}
+              onClick={() => handleTabClick(index, tab.query)}
+              className={`cursor-pointer font-medium text-[12.5px] sm:text-sm px-2.5 sm:px-4 py-1 sm:py-2 rounded-full mb-2 md:mb-0 ${
+                activeTab === index
+                  ? "text-white bg-accent"
+                  : "text-gray-700 hover:text-gray-900"
+              }`}
+            >
+              {tab.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TabsSection;
