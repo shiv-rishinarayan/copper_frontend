e24b1e502f3173eaa2e77d8f9833198b7fd05eb7
Vaibhav <vaibhavjam86@gmail.com>
2025-01-03 14:09:07 +0530

    remove loader spin code

--- components/Home/LatestNews.js
+++ components/Home/LatestNews.js
@@ -150,7 +150,6 @@ const LatestNews = () => {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-32">
-        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-800"></div>
         <span className="ml-3 text-gray-800 font-semibold mx-auto">
           <Loader />
         </span>
