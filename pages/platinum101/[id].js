68dd94cce4ac5555e424f6f3797fc9e4df8fe7aa
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2025-01-09 15:15:07 +0530

    dynamic p101 page style updated

--- pages/platinum101/[id].js
+++ pages/platinum101/[id].js
@@ -2,6 +2,7 @@ import P101Breadcrumb from "@/components/P101/P101Breadcrumb";
 import { useRouter } from "next/router";
 import cardData from "@/public/static-data/P101CardData"; // Adjust the import path
 import Image from "next/image";
+import Footer from "@/components/Footer";
 
 const DetailPage = () => {
   const router = useRouter();
@@ -22,20 +23,29 @@ const DetailPage = () => {
       {/* Page Content */}
       <div className="flex flex-col mt-[3.5rem]">
         {/* header  */}
-        <div className="bg-accent/10 px-3 md:px-20 py-16 mb-16">
-          <Image
-            src={card.image}
-            alt={card.title}
-            width={200}
-            height={200}
-            className="rounded-lg mb-4 -ml-[4px]"
-          />
-          <h1 className="text-[28px] font-bold mb-2 pl-[2px]">{card.title}</h1>
-          <p className="text-[17px] text-gray-700 mb-4 pl-[2px]">{card.description}</p>
+        <div className="bg-accent/10 px-3 md:px-20 py-7 md:py-16 mb-16">
+          <div className="w-full lg:w-[70%] mx-auto">
+            <Image
+              src={card.image}
+              alt={card.title}
+              width={200}
+              height={200}
+              className="rounded-lg mb-4 -ml-[4px]"
+            />
+            <h1 className="text-[28px] font-bold mb-2 pl-[2px]">
+              {card.title}
+            </h1>
+            <p className="text-[17px] text-gray-700 mb-4 pl-[2px]">
+              {card.description}
+            </p>
+          </div>
         </div>
         {/* Render detailed sections dynamically */}
         {card.details.map((section, index) => (
-          <div key={index} className="mb-10 px-3 md:px-20">
+          <div
+            key={index}
+            className="mb-10 px-3 md:px-20 w-full lg:w-[70%] mx-auto"
+          >
             <h2 className="text-[22px] font-bold mb-2">{section.heading}</h2>
             <ul className="list-none pl-0">
               {section.content.map((item, idx) => (
@@ -46,6 +56,11 @@ const DetailPage = () => {
             </ul>
           </div>
         ))}
+      </div>
+
+      {/* footer  */}
+      <div className="mt-7 md:mt-16">
+        <Footer />
       </div>
     </div>
   );
