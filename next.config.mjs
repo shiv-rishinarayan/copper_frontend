5ab96b25193208cd69e50cfaeb9ce5a35daa9a82
Vaibhav <vaibhavjam86@gmail.com>
2025-01-10 11:15:12 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["platinumdjango-production.up.railway.app"],
+  },
 };
 
 export default nextConfig;
