338dff946f806d01d8621a465b8bfacc5c6ca853
Yogesh <coolcoder9334@gmail.com>
2025-01-21 20:29:07 +0530

    Updated the chart style

--- components/Investment/ETF/Charts/EuropeChart.js
+++ components/Investment/ETF/Charts/EuropeChart.js
@@ -1,3 +1,100 @@
+// import React, { useEffect, useRef, memo } from "react";
+
+// function TradingViewWidget() {
+//   const container = useRef();
+
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.src =
+//       "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
+//     script.type = "text/javascript";
+//     script.async = true;
+//     script.innerHTML = `
+//         {
+//           "symbols": [
+//             [
+//               "LSE:IPLT|ALL"
+//             ],
+//             [
+//               "LSE:SPPT|ALL"
+//             ],
+//             [
+//               "EURONEXT:PHPT|ALL"
+//             ],
+//             [
+//               "MIL:XAD3|ALL"
+//             ],
+//             [
+//               "LSE:XPPT|ALL"
+//             ],
+//             [
+//               "TURQUOISE:PTCHAZ|ALL"
+//             ],
+//             [
+//               "SIX:ZPLA|ALL"
+//             ],
+//             [
+//               "LSE:PHPM|ALL"
+//             ]
+//           ],
+//           "chartOnly": false,
+//           "width": "100%",
+//           "height": "100%",
+//           "locale": "en",
+//           "colorTheme": "light",
+//           "autosize": true,
+//           "showVolume": false,
+//           "showMA": false,
+//           "hideDateRanges": false,
+//           "hideMarketStatus": false,
+//           "hideSymbolLogo": false,
+//           "scalePosition": "right",
+//           "scaleMode": "Normal",
+//           "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+//           "fontSize": "10",
+//           "noTimeScale": false,
+//           "valuesTracking": "1",
+//           "changeMode": "price-and-percent",
+//           "chartType": "area",
+//           "maLineColor": "#2962FF",
+//           "maLineWidth": 1,
+//           "maLength": 9,
+//           "headerFontSize": "medium",
+//           "lineWidth": 2,
+//           "lineType": 0,
+//           "dateRanges": [
+//             "1d|1",
+//             "1m|30",
+//             "3m|60",
+//             "12m|1D",
+//             "60m|1W",
+//             "all|1M"
+//           ]
+//         }`;
+//     container.current.appendChild(script);
+//   }, []);
+
+//   return (
+//     <div className="tradingview-widget-container" ref={container}>
+//       <div className="tradingview-widget-container__widget"></div>
+//       <div className="tradingview-widget-copyright">
+//         {/* <a
+//           href="https://www.tradingview.com/"
+//           rel="noopener nofollow"
+//           target="_blank"
+//         >
+//           <span className="blue-text">Track all markets on TradingView</span>
+//         </a> */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default memo(TradingViewWidget);
+
+//.....................................
+
+// TradingViewWidget.jsx
 import React, { useEffect, useRef, memo } from "react";
 
 function TradingViewWidget() {
@@ -6,77 +103,72 @@ function TradingViewWidget() {
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
     script.type = "text/javascript";
     script.async = true;
     script.innerHTML = `
         {
-          "symbols": [
-            [
-              "LSE:IPLT|ALL"
-            ],
-            [
-              "LSE:SPPT|ALL"
-            ],
-            [
-              "EURONEXT:PHPT|ALL"
-            ],
-            [
-              "MIL:XAD3|ALL"
-            ],
-            [
-              "LSE:XPPT|ALL"
-            ],
-            [
-              "TURQUOISE:PTCHAZ|ALL"
-            ],
-            [
-              "SIX:ZPLA|ALL"
-            ],
-            [
-              "LSE:PHPM|ALL"
-            ]
-          ],
-          "chartOnly": false,
-          "width": "100%",
-          "height": "100%",
-          "locale": "en",
-          "colorTheme": "light",
           "autosize": true,
-          "showVolume": false,
-          "showMA": false,
-          "hideDateRanges": false,
-          "hideMarketStatus": false,
-          "hideSymbolLogo": false,
-          "scalePosition": "right",
-          "scaleMode": "Normal",
-          "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-          "fontSize": "10",
-          "noTimeScale": false,
-          "valuesTracking": "1",
-          "changeMode": "price-and-percent",
-          "chartType": "area",
-          "maLineColor": "#2962FF",
-          "maLineWidth": 1,
-          "maLength": 9,
-          "headerFontSize": "medium",
-          "lineWidth": 2,
-          "lineType": 0,
-          "dateRanges": [
-            "1d|1",
-            "1m|30",
-            "3m|60",
-            "12m|1D",
-            "60m|1W",
-            "all|1M"
-          ]
+          "symbol": "MIL:XAD3",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "light",
+          "style": "2",
+          "locale": "en",
+          "hide_top_toolbar": true,
+          "allow_symbol_change": false,
+          "save_image": false,
+          "compareSymbols": [
+            {
+              "symbol": "LSE:SPPT",
+              "position": "SameScale"
+            },
+            {
+              "symbol": "LSE:PHPT",
+              "position": "SameScale"
+            },
+            {
+              "symbol": "LSE:XPPT",
+              "position": "SameScale"
+            },
+            {
+              "symbol": "MIL:XPPE",
+              "position": "SameScale"
+            },
+            {
+              "symbol": "TURQUOISE:PTCHAZ",
+              "position": "SameScale"
+            },
+            {
+              "symbol": "SIX:ZPLA",
+              "position": "SameScale"
+            },
+            {
+              "symbol": "LSE:PHPM",
+              "position": "SameScale"
+            },
+            {
+              "symbol": "LSE:IPLT",
+              "position": "SameScale"
+            }
+          ],
+          "calendar": false,
+          "hide_volume": true,
+          "support_host": "https://www.tradingview.com"
         }`;
     container.current.appendChild(script);
   }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
+    <div
+      className="tradingview-widget-container"
+      ref={container}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <div
+        className="tradingview-widget-container__widget"
+        style={{ height: "calc(100% - 32px)", width: "100%" }}
+      ></div>
       <div className="tradingview-widget-copyright">
         {/* <a
           href="https://www.tradingview.com/"
