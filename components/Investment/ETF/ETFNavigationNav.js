0ccefe84ff0c2f7d1b812d81d8aad65f51baec2a
Yogesh <coolcoder9334@gmail.com>
2025-01-24 15:42:19 +0530

    resized nav

--- components/Investment/ETF/ETFNavigationNav.js
+++ components/Investment/ETF/ETFNavigationNav.js
@@ -3,14 +3,14 @@ import { ETFdata } from "./ETFdata";
 
 const ETFNavigationNav = ({ onRegionSelect, selectedRegion }) => {
   return (
-    <div className="flex flex-wrap gap-5 sm:gap-4 mb-8 justify-center">
+    <div className="flex flex-wrap gap-5 sm:gap-4 mb-8 justify-center ">
       {ETFdata.regions.map((region) => {
         const isActive = region.name.toLowerCase() === selectedRegion;
         return (
           <button
             key={region.name}
             onClick={() => onRegionSelect(region.name)}
-            className={` sm:px-6 py-1 px-3 rounded-full transition-colors text-sm sm:text-base ${
+            className={`py-1 sm:px-4 px-3 rounded-full transition-colors text-sm sm:text-base ${
               isActive
                 ? "bg-accent text-white"
                 : " text-black hover:bg-accent/20"
