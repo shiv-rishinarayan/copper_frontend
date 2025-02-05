99b95f359e111d0c69c9eaaaf4981f48bf35a83e
Vaibhav <vaibhavjam86@gmail.com>
2025-02-05 15:19:14 +0530

    added all apis in one file

---
+++ src/api/platinumAPI.js
@@ -0,0 +1,11 @@
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASEURL;
+
+export const PLATINUM_NEWS = `${BASE_URL}api/platinum_news/`;
+
+export const SPORT_PRICE_CHART = `${BASE_URL}api/spot-price-chart/`;
+
+export const PGM_PRICES = `${BASE_URL}api/pgm-prices/`;
+
+export const PRESS_RELEASE = `${BASE_URL}api/press-releases/`;
+
+export const STOCK_NEWS = `${BASE_URL}api/stock-news/`;
