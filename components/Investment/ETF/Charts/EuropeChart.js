6fb05c002a3daab621a2411dbb70405b6ca6fb52
Vaibhav <vaibhavjam86@gmail.com>
2025-02-06 12:31:55 +0530

    remove iplt stock

--- components/Investment/ETF/Charts/EuropeChart.js
+++ components/Investment/ETF/Charts/EuropeChart.js
@@ -38,10 +38,7 @@ function TradingViewWidget() {
               "position": "SameScale"
             },
            
-            {
-              "symbol": "LSE:IPLT",
-              "position": "SameScale"
-            }
+            
           ],
           "calendar": false,
           "hide_volume": true,
