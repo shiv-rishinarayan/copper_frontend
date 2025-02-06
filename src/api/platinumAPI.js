dc2af666c14d60a9fd08bc188747031fec024a27
Vaibhav <vaibhavjam86@gmail.com>
2025-02-06 11:06:35 +0530

    C:/Program Files/Git/ added at endpoint

--- src/api/platinumAPI.js
+++ src/api/platinumAPI.js
@@ -2,32 +2,32 @@ export const BASE_URL = process.env.NEXT_PUBLIC_API_BASEURL;
 
 //HOME & NEWS APIS
 
-export const PLATINUM_NEWS = `${BASE_URL}api/platinum_news/`;
+export const PLATINUM_NEWS = `${BASE_URL}/api/platinum_news/`;
 
-export const SPORT_PRICE_CHART = `${BASE_URL}api/spot-price-chart/`;
+export const SPORT_PRICE_CHART = `${BASE_URL}/api/spot-price-chart/`;
 
-export const PGM_PRICES = `${BASE_URL}api/pgm-prices/`;
+export const PGM_PRICES = `${BASE_URL}/api/pgm-prices/`;
 
-export const PRESS_RELEASE = `${BASE_URL}api/press-releases/`;
+export const PRESS_RELEASE = `${BASE_URL}/api/press-releases/`;
 
-export const STOCK_NEWS = `${BASE_URL}api/stock-news/`;
+export const STOCK_NEWS = `${BASE_URL}/api/stock-news/`;
 
 //INVESTMENT APIS
 
-export const MOST_FOLLOWED = `${BASE_URL}api/most-followed-stocks/`;
+export const MOST_FOLLOWED = `${BASE_URL}/api/most-followed-stocks/`;
 
-export const PGM_STOCK_DETAIL = `${BASE_URL}api/pgm-stock-detail/`;
+export const PGM_STOCK_DETAIL = `${BASE_URL}/api/pgm-stock-detail/`;
 
-export const STOCK_SCREENER = `${BASE_URL}api/stock-metrics/`;
+export const STOCK_SCREENER = `${BASE_URL}/api/stock-metrics/`;
 
-export const FOLLOWED_STOCKS = `${BASE_URL}api/followed-stocks/`;
+export const FOLLOWED_STOCKS = `${BASE_URL}/api/followed-stocks/`;
 
-export const INSIDER_TRANSACTIONS = `${BASE_URL}api/insider-transactions/`;
+export const INSIDER_TRANSACTIONS = `${BASE_URL}/api/insider-transactions/`;
 
 //VIDEOS API
 
-export const VIDEOS = `${BASE_URL}api/videos/`;
+export const VIDEOS = `${BASE_URL}/api/videos/`;
 
 //CALENDAR
 
-export const CALENDAR = `${BASE_URL}api/calendar-events`;
+export const CALENDAR = `${BASE_URL}/api/calendar-events`;
